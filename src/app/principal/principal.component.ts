2361e83f5845812eda79a137404fd3b9528b99ed
Victor Vidigal Ribeiro <victor.ribeiro1@granbery.edu.dti>
2023-10-03 21:49:32 -0300

    recuperando filmes favoritos

--- src/app/principal/principal.component.ts
+++ src/app/principal/principal.component.ts
@@ -8,27 +8,35 @@ import { PrincipalService } from './principal.service';
 })
 export class PrincipalComponent implements OnInit {
 
-  id = '';
-  original_title = '';
-  homepage = '';
-  overview = '';
-  imgSrc = 'https://image.tmdb.org/t/p/w220_and_h330_face/'
+  filmes = [];
 
   constructor(private principalService: PrincipalService) {}
 
   ngOnInit(): void {
-    this.principalService.listarFilmes().subscribe(data => {
-      this.id = data.id;
-      this.original_title = data.original_title;
-      this.homepage = data.homepage;
-      this.overview = data.overview;
-
-      this.principalService.listarImagens('615656').subscribe(img => {
-        console.log(img);
-        this.imgSrc = this.imgSrc + img.posters[0].file_path;
-      });
-
+    
+    this.principalService.listarFilmesFavoritos().subscribe(data => {
+      this.filmes = data.results;
     });
+
   }
 
 }
+
+
+
+
+
+
+
+// this.principalService.listarFilmes().subscribe(data => {
+    //   this.id = data.id;
+    //   this.original_title = data.original_title;
+    //   this.homepage = data.homepage;
+    //   this.overview = data.overview;
+
+    //   this.principalService.listarImagens('615656').subscribe(img => {
+    //     console.log(img);
+    //     this.imgSrc = this.imgSrc + img.posters[0].file_path;
+    //   });
+
+    // });
--- src/app/principal/principal.service.ts
+++ src/app/principal/principal.service.ts
@@ -9,27 +9,41 @@ export class PrincipalService {
 
   constructor(private http: HttpClient) { }
 
-  listarFilmes(): Observable<any> {
-    const httpOptions = {
-      headers: new HttpHeaders({
-        'Content-Type':  'application/json',
-        'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3YjFjM2M5OTYxYTUyNWQ2OWQ4MTI1ZTFhZGY5NzBiNyIsInN1YiI6IjYyOWZlNWNlMzVkMWJjMDA5YjU3NzdlMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.zBrUteoQPPsjlA4SjGddHnB5t_bFMobHOsNdUAbkzYw'
-      })
-    };
+  listarFilmesFavoritos(): Observable<any> {
+    const options = {
+          headers: new HttpHeaders({
+            'Content-Type':  'application/json',
+            'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3YjFjM2M5OTYxYTUyNWQ2OWQ4MTI1ZTFhZGY5NzBiNyIsInN1YiI6IjYyOWZlNWNlMzVkMWJjMDA5YjU3NzdlMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.zBrUteoQPPsjlA4SjGddHnB5t_bFMobHOsNdUAbkzYw'
+          })
+        };
 
-    return this.http.get('https://api.themoviedb.org/3/movie/latest', httpOptions)
+
+    return this.http.get("https://api.themoviedb.org/3/account/"+12634572+"/favorite/movies", options);
   }
 
-  listarImagens(movieId: string): Observable<any> {
-    const httpOptions = {
-      headers: new HttpHeaders({
-        'Content-Type':  'application/json',
-        'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3YjFjM2M5OTYxYTUyNWQ2OWQ4MTI1ZTFhZGY5NzBiNyIsInN1YiI6IjYyOWZlNWNlMzVkMWJjMDA5YjU3NzdlMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.zBrUteoQPPsjlA4SjGddHnB5t_bFMobHOsNdUAbkzYw'
-      })
-    };
 
-    return this.http.get('https://api.themoviedb.org/3/movie/'+movieId+'/images', httpOptions)
-  }
+
+  // listarFilmes(): Observable<any> {
+  //   const httpOptions = {
+  //     headers: new HttpHeaders({
+  //       'Content-Type':  'application/json',
+  //       'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3YjFjM2M5OTYxYTUyNWQ2OWQ4MTI1ZTFhZGY5NzBiNyIsInN1YiI6IjYyOWZlNWNlMzVkMWJjMDA5YjU3NzdlMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.zBrUteoQPPsjlA4SjGddHnB5t_bFMobHOsNdUAbkzYw'
+  //     })
+  //   };
+
+  //   return this.http.get('https://api.themoviedb.org/3/movie/latest', httpOptions)
+  // }
+
+  // listarImagens(movieId: string): Observable<any> {
+  //   const httpOptions = {
+  //     headers: new HttpHeaders({
+  //       'Content-Type':  'application/json',
+  //       'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3YjFjM2M5OTYxYTUyNWQ2OWQ4MTI1ZTFhZGY5NzBiNyIsInN1YiI6IjYyOWZlNWNlMzVkMWJjMDA5YjU3NzdlMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.zBrUteoQPPsjlA4SjGddHnB5t_bFMobHOsNdUAbkzYw'
+  //     })
+  //   };
+
+  //   return this.http.get('https://api.themoviedb.org/3/movie/'+movieId+'/images', httpOptions)
+  // }
 
 }
 
